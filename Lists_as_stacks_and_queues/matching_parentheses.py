436ea9c2b5d5de15d094bdde8203ff5e8e4ba6be
waskoooo <vasiltarashev@gmail.com>
2024-04-18 13:40:04 +0300

    1

---
+++ Lists_as_stacks_and_queues/matching_parentheses.py
@@ -0,0 +1,12 @@
+expression = input()
+paren_index = []
+
+for index in range(0, len(expression)):
+    if expression[index] == '(':
+        paren_index.append(index)
+    elif expression[index] == ')':
+        start_index = paren_index.pop()
+        end_index = index + 1
+        print(expression[start_index:end_index])
+
+        
