00653aa28d385b5d73e4a7eedf4c457ad013d27e
waskoooo <vasiltarashev@gmail.com>
2024-04-21 15:54:11 +0300

    1

---
+++ Lists_as_stacks_and_queus_ex/Fast_food.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+food = int(input())
+orders = deque([int(x) for x in input().split()])
+
+print(max(orders))
+
+# for order in orders.copy():
+#     if food >= order:
+#         orders.popleft()
+#         food -= order
+#
+#     else:
+#         print(f"Orders left:", *orders)
+#         break
+#
+# else:
+#     print(f"Orders complete")
+
+#  second solution :
+
+while orders:
+    order = orders.popleft()
+
+    if food >= order:
+        food -= order
+    else:
+        print(f"Orders left:", order, *orders)
+        break
+else:
+    print("Orders complete")
