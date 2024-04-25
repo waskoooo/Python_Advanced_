d001bb8d5423fb4a108524d6c7d84bb0f4340882
waskoooo <vasiltarashev@gmail.com>
2024-04-25 15:15:52 +0300

    1

---
+++ Multidimensional_lists/sum_matrix_elements.py
@@ -0,0 +1,11 @@
+row, cow = [int(el) for el in input().split(', ')]
+
+total_amount = 0
+matrix = []
+for i in range(row):
+    row_data = [int(el) for el in input().split(', ')]
+    total_amount += sum(row_data)
+    matrix.append(row_data)
+
+print(total_amount)
+print(matrix)
