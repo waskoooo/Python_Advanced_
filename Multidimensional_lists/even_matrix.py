a7bf73c75f0b3a4139b9b12ef6e29f6fe5adfab9
waskoooo <vasiltarashev@gmail.com>
2024-04-25 15:41:32 +0300

    1

--- Multidimensional_lists/even_matrix.py
+++ Multidimensional_lists/even_matrix.py
@@ -3,16 +3,16 @@ row_count = int(input())
 matrix = []
 
 for i in range(row_count):
-    data = [int(el) for el in input().split(', ')]
+    data = [int(el) for el in input().split(', ') if int(el) % 2 == 0]
     matrix.append(data)
 
-even_matrix = []
+# even_matrix = []
 
-for row in matrix:
-    sub_list = []
-    for el in row:
-        if el % 2 == 0:
-            sub_list.append(el)
-    even_matrix.append(sub_list)
+# for row in matrix:
+#     sub_list = []
+#     for el in row:
+#         if el % 2 == 0:
+#             sub_list.append(el)
+#     even_matrix.append(sub_list)
 
-print(even_matrix)
+print(matrix)
