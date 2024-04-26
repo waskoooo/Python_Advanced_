3f98b8443b678b6196e7ac8d7bac6c504c75d2ce
waskoooo <vasiltarashev@gmail.com>
2024-04-26 10:46:56 +0300

    1

--- Multidimensional_lists/flattening_matrix.py
+++ Multidimensional_lists/flattening_matrix.py
@@ -1,17 +1,21 @@
 row = int(input())
+# 1.type
+# flatten = []
 
-flatten = []
+# for i in range(row):
+#     row_data = [int(el) for el in input().split()]
+#     flatten.extend(row_data)
+# print(flatten)
 
-for i in range(row):
-    row_data = [int(el) for el in input().split()]
-    flatten.extend(row_data)
-print(flatten)
-
-
+# 2.type:
 # flatten = []
 # for row in matrix:
 #     for el in row:
 #         flatten.append(el)
 
 # flatten = [el for row in matrix for el in row]
+# 3.type
 
+matrix = [map(int, input().split(', ')) for _ in range(row)]
+flattened = [num for row in matrix for num in row]
+print(flattened)
