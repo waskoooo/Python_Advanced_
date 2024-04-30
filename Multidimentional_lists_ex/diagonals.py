b26912570e4757e34f378ae072558bd3245edc34
waskoooo <vasiltarashev@gmail.com>
2024-04-30 12:32:04 +0300

    1

--- Multidimentional_lists_ex/diagonals.py
+++ Multidimentional_lists_ex/diagonals.py
@@ -8,3 +8,4 @@ second = [matrix[r][n - r - 1] for r in range(n)]
 
 print(f"Primary diagonal: {', '.join(str(x) for x in primary)}. Sum: {sum(primary)}")
 print(f"Secondary diagonal: {', '.join(str(x) for x in second)}. Sum: {sum(second)}")
+
