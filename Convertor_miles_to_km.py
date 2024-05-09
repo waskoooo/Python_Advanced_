6d207cc13e1d7f5f4ab7068801240c5c34fe3411
waskoooo <vasiltarashev@gmail.com>
2024-05-09 13:20:59 +0300

    1

---
+++ Convertor_miles_to_km.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+import ttkbootstrap as ttk
+
+
+def convert():
+    mile_input = entry_int.get()
+    km_output = mile_input * 1.61
+    output_string.set(km_output)
+
+
+# window
+window = ttk.Window(themename='journal')
+window.title('Miles to KM ')
+window.geometry('400x150')
+
+# title
+title_label = ttk.Label(master=window,
+                        text='Miles to Kilometers',
+                        font='Ariel 28 bold')
+title_label.pack()
+
+# input field
+input_frame = ttk.Frame(master=window)
+entry_int = tk.IntVar()
+entry = ttk.Entry(master=input_frame, textvariable=entry_int)
+button = ttk.Button(master=input_frame,
+                    text=' Convert',
+                    command=convert)
+entry.pack(side='left', padx=10)
+button.pack(side='left')
+input_frame.pack(pady=15)
+
+# output
+output_string = tk.StringVar()
+output_label = ttk.Label(master=window,
+                         text='Output',
+                         font='Ariel 24',
+                         textvariable= output_string)
+output_label.pack(pady=5)
+
+# run
+window.mainloop()
