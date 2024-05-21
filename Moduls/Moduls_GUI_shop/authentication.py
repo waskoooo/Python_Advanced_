30a6fec203a7a6fbbf3df8d3e1b6d0b07d70d184
waskoooo <vasiltarashev@gmail.com>
2024-05-21 12:23:37 +0300

    1

--- Moduls/Moduls_GUI_shop/authentication.py
+++ Moduls/Moduls_GUI_shop/authentication.py
@@ -1,4 +1,9 @@
+from tkinter import Entry
+
 from tkmacosx import Button
+
+from Moduls.Moduls_GUI_shop.helpers import clean_screen
+
 from canvas import root, frame
 
 
@@ -25,11 +30,46 @@ def render_entry():
     )
 
     frame.create_window(350, 260, window=register_button)
+    frame.create_window(350, 310, window=login_button)
 
 
 def register():
-    print("Register")
+    clean_screen()
+
+    frame.create_text(100, 50, text="First Name")
+    frame.create_text(100, 100, text="Last Name")
+    frame.create_text(100, 150, text="User Name")
+    frame.create_text(100, 200, text="Password")
+
+    frame.create_window(230, 50, window=first_name_box)
+    frame.create_window(230, 100, window=last_name_box)
+    frame.create_window(230, 150, window=username_box)
+    frame.create_window(230, 200, window=password_box)
+
+    register_button = Button(
+        root,
+        text="Register",
+        bg="green",
+        fg="white",
+        width=80,
+        height=40,
+        bd=0,
+        command=registration
+    )
+
+    frame.create_window(315, 250, window=register_button)
+
+
+def registration():
+    print("Registered")
 
 
 def login():
+    clean_screen()
     print('Login')
+
+
+first_name_box = Entry(root, bd=0)
+last_name_box = Entry(root, bd=0)
+username_box = Entry(root, bd=0)
+password_box = Entry(root, bd=0)
